ece1a0a0473bd6dd1a9894371f5be327712877f2
Yash Raj Jain <yash.r.jain22@gmail.com>
2026-07-05 17:41:48 +0530

    .

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,6 +1,6 @@
 export const cookieOptions = {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
+    secure: true ,
+    sameSite: "none",
     maxAge: 1000 * 60 * 5, // 5 minutes
 };
