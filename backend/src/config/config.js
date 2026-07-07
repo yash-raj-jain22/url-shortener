30df9071098ab8070c2e8273bd2f7eabb1393ab7
Yash Raj Jain <yash.r.jain22@gmail.com>
2026-07-07 14:55:49 +0530

    changed the time to expire the cookie

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,6 +1,6 @@
 export const cookieOptions = {
     httpOnly: true,
-    secure: true ,
+    secure: true,
     sameSite: "none",
-    maxAge: 1000 * 60 * 5, // 5 minutes
+    maxAge: 1000 * 60 * 60 * 24 * 1, // 1 day
 };
